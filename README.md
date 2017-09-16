3d160828d6e72d073229e99b5cfab75d85ed38e1
raryosu <me@raryosu.info>
2017-09-16 23:33:16 +0900

    Fix dbname and so on

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ## How to develop?
 
 ```
-$ docker-compoose up
+$ docker-compoose up [-d]
 ```
+
+!Warning! 初回起動時はMySQLの設定が終了する前にWebコンテナが起動する可能性があります．
+適切に動作しない場合は，一度 docker-compose を停止した後に起動してください．
--- tracking/settings.py
+++ tracking/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tracking',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'tracking',
+        'PASSWORD': 'tracking',
         'HOST': 'mysql',
         'PORT': 3306,
     }
