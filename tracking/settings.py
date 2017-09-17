a36dc57a30dd35886e8bb1efbb61e93bc5e7fb98
raryosu <me@raryosu.info>
2017-09-17 12:10:11 +0900

    MySQLの設定を環境変数読むように変更

--- tracking/settings.py
+++ tracking/settings.py
@@ -77,9 +77,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tracking',
-        'USER': 'tracking',
-        'PASSWORD': 'tracking',
-        'HOST': 'mysql',
+        'USER': os.getenv("MYSQL_USER", "root"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD", "root"),
+        'HOST': os.getenv("MYSQL_HOST", "127.0.0.1"),
         'PORT': 3306,
     }
 }
